69f636f1c4658f430146b34c240cb76b168af191
JFurB <dimabondarenko471@gmail.com>
2025-11-11 18:15:33 +0300

    Добавил док строчки для объяснения ошибки service Orders

--- service_orders/errors.py
+++ service_orders/errors.py
@@ -1,5 +1,6 @@
 
 class ApiException(Exception):
+    """Кастомная ошибка для нашего API"""
     def __init__(self, code: str, message: str):
         self.code = code
         self.message = message
